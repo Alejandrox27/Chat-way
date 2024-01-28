229063b39ad891188482a0a0d0c54581d62e7f37
alx_27 <didieralejandromejia83@gmail.com>
2024-01-28 16:42:14 -0500

    Add connection to firebase and CDN's

---
+++ app.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js'
+import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCLUZ2wZxw3VfU1VWNgKAnpcSEtY8-0fiU",
+  authDomain: "firechat-bootstrap-b07e1.firebaseapp.com",
+  projectId: "firechat-bootstrap-b07e1",
+  storageBucket: "firechat-bootstrap-b07e1.appspot.com",
+  messagingSenderId: "1078533434003",
+  appId: "1:1078533434003:web:34bbe395eab1a2dfffbb66"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
